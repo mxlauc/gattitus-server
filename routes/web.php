d68c7bbf77e2d68acbf38101824968db4278cc8d
Marx L. Canaza <denotefull@gmail.com>
2021-08-21 16:11:08 -0500

    Agregar login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
+use Laravel\Socialite\Facades\Socialite;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +20,37 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/auth/login/facebook', function () {
+    return Socialite::driver('facebook')->redirect();
+})->name("login.facebook");
+
+
+Route::get('/auth/callback/facebook', function () {
+    $user_social = Socialite::driver('facebook')->user();
+
+    $user = User::where('facebook_id', $user_social->getId())->first();
+
+    //$user_social->token,
+
+    if(!$user){
+        $url_imagen = "https://graph.facebook.com/v3.3/{$user->id}/picture?type=small&access_token={$user->token}";
+        $fileContents = file_get_contents($url_imagen);
+        //File::put(public_path() . '/storage/usuarios_imagen/' . $user->getId() . ".jpg", $fileContents);
+        $path_image = $user_social->getId() . "_" . round(microtime(true) * 1000) . "_" . rand(30000, 60000) . ".jpg";
+        Storage::put($path_image, $fileContents);
+        $user = User::create([
+            'name'          => $user_social->getName(),
+            'email'         => $user_social->getEmail(),
+            'avatar'        => $path_image,
+            'facebook_id'   => $user_social->getId(),
+        ]);
+    }
+
+    loginAndRedirect($user);
+});
+
+function loginAndRedirect($user){
+    Auth::login($user);
+    return redirect("/");
+}
