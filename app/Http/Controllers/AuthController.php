eebf2e02fc91a1fe6d480328417d94dd1fc60161
Marx L. Canaza <denotefull@gmail.com>
2021-08-21 17:04:10 -0500

    Crear auth controller

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Laravel\Socialite\Facades\Socialite;
+
+class AuthController extends Controller
+{
+    public function loginFacebook(){
+        return Socialite::driver('facebook')->redirect();
+    }
+
+    public function callbackFacebook(){
+        $user_social = Socialite::driver('facebook')->user();
+        $user = User::where('facebook_id', $user_social->getId())->first();
+
+        if(!$user){
+            $url_imagen = "https://graph.facebook.com/v3.3/{$user_social->id}/picture?type=small&access_token={$user_social->token}";
+            $fileContents = file_get_contents($url_imagen);
+            $path_image = $user_social->getId() . "_" . round(microtime(true) * 1000) . "_" . rand(30000, 60000) . ".jpg";
+            Storage::put($path_image, $fileContents);
+            $user = User::create([
+                'name'          => $user_social->getName(),
+                'email'         => $user_social->getEmail(),
+                'avatar'        => $path_image,
+                'facebook_id'   => $user_social->getId(),
+            ]);
+        }
+
+        $this->login($user);
+    }
+
+    function login($user){
+        Auth::login($user);
+        return redirect("/");
+    }
+
+    public function logout(){
+        Auth::logout();
+        return redirect("/");
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Models\User;
-use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Storage;
 use Laravel\Socialite\Facades\Socialite;
 
 /*
@@ -21,36 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/auth/login/facebook', function () {
-    return Socialite::driver('facebook')->redirect();
-})->name("login.facebook");
+Route::get('/auth/login/facebook', [AuthController::class, 'loginFacebook'])->name("login.facebook");
+Route::get('/auth/callback/facebook', [AuthController::class, 'callbackFacebook']);
+Route::post('/auth/logout', [AuthController::class, 'logout']);
 
-
-Route::get('/auth/callback/facebook', function () {
-    $user_social = Socialite::driver('facebook')->user();
-
-    $user = User::where('facebook_id', $user_social->getId())->first();
-
-    //$user_social->token,
-
-    if(!$user){
-        $url_imagen = "https://graph.facebook.com/v3.3/{$user_social->id}/picture?type=small&access_token={$user_social->token}";
-        $fileContents = file_get_contents($url_imagen);
-        //File::put(public_path() . '/storage/usuarios_imagen/' . $user->getId() . ".jpg", $fileContents);
-        $path_image = $user_social->getId() . "_" . round(microtime(true) * 1000) . "_" . rand(30000, 60000) . ".jpg";
-        Storage::put($path_image, $fileContents);
-        $user = User::create([
-            'name'          => $user_social->getName(),
-            'email'         => $user_social->getEmail(),
-            'avatar'        => $path_image,
-            'facebook_id'   => $user_social->getId(),
-        ]);
-    }
-
-    loginAndRedirect($user);
-});
-
-function loginAndRedirect($user){
-    Auth::login($user);
-    return redirect("/");
-}
