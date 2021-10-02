3f64261b28c10dedb3ce909c700bba5df2cddb94
Amilcar Laura <denotefull@gmail.com>
2021-10-02 11:36:08 -0500

    Agregar Firebase Admin

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
-
+use Ramsey\Uuid\Uuid;
+use Intervention\Image\Facades\Image;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,6 +16,49 @@ use Laravel\Socialite\Facades\Socialite;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::post('images/upload', function(Request $request){
+    $bucket = app('firebase.storage')->getBucket();
+    $uuid = Uuid::uuid4()->toString();
+    $name = 'img.jpg';
+
+    $bucket->upload(fopen($request->file('file')->getRealPath(), 'r'), [
+        'name' => $name,
+        'metadata' => [
+            'metadata' => [
+                'firebaseStorageDownloadTokens' => $uuid
+            ]
+        ]
+    ]);
+    
+    
+    // open an image file
+    $img = Image::make($request->file('file')->getRealPath());
+
+    // now you are able to resize the instance
+    $img->resize(200, 200, function ($constraint) {
+        $constraint->aspectRatio();
+        $constraint->upsize();
+    });
+
+    // finally we save the image as a new file
+    $img->save(sys_get_temp_dir() . $uuid);
+
+    $name = 'xd' . $name;
+
+    $bucket->upload(fopen(sys_get_temp_dir() . $uuid, 'r'), [
+        'name' => $name,
+        'metadata' => [
+            'metadata' => [
+                'firebaseStorageDownloadTokens' => $uuid
+            ]
+        ]
+    ]);
+    return [
+        "estado" => "ok",
+        "url" => "https://firebasestorage.googleapis.com/v0/b/{$bucket->name()}/o/{$name}?alt=media&token={$uuid}"
+    ];
+});
 
 Route::get('/', function () {
     return view('pages.home');
@@ -26,3 +71,7 @@ Route::post('/auth/logout', [AuthController::class, 'logout'])->name('logout');
 Route::get('editor', function(){
     return view('pages.editor');
 })->name('editor');
+
+Route::get('hey.js', function(){
+    return 'jeje';
+});
