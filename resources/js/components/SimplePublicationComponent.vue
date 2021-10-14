4ca13ab1dfe352c3bcdc72d37dda7853562a0008
Amilcar Laura <denotefull@gmail.com>
2021-10-14 18:32:38 -0500

    Cambiar aspect_ratio

--- resources/js/components/SimplePublicationComponent.vue
+++ resources/js/components/SimplePublicationComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card card-publication rounded-5 border-0 shadow f-rubick mb-4">
-    <div class="card-body">
+    <div class="card-body pb-0">
       <div class="row g-0">
         <div class="col-auto">
           <img
@@ -29,46 +29,43 @@
         </div>
       </div>
         <div class="rounded-5 img-container-publication shadow-sm my-2 position-relative"
-        :style="{ aspectRatio: publication.image.aspect_ratio, background: `linear-gradient(45deg, ${publication.image.color_bl} 0%, ${publication.image.color_tr} 100%)`}">
+        :style="{ aspectRatio: publication.image.aspect_ratio < 0.8 ? 0.8 : publication.image.aspect_ratio, background: `linear-gradient(45deg, ${publication.image.color_bl} 0%, ${publication.image.color_tr} 100%)`}">
           <img class="img-publication img-fluid w-100 shadow-sm opacity-0"
             :src="publication.image.url"
+            :style="{ aspectRatio: publication.image.aspect_ratio < 0.8 ? 0.8 : publication.image.aspect_ratio}"
             @load="onLoadImage"/>
             <div class="position-absolute top-0 start-0 end-0 bottom-0"></div>
         </div>
         
-        <p class="fs-6 text-muted">
+        <p class="fs-6 mb-2 text-muted">
           {{publication.description}}
         </p>
-        <hr class="mt-0 mb-2">
-        <svg xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            class="imgAction"
-            fill="currentColor"
-            viewBox="0 0 16 16">
-            <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
-            <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
-          </svg>
-          <svg xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            class="imgAction"
-            fill="currentColor"
-            viewBox="0 0 16 16">
-            <path v-if="true"
-                fill-rule="evenodd"
-                d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
-            <path v-else
-                d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
-          </svg>
-          <svg xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            class="imgAction"
-            fill="currentColor"
-            viewBox="0 0 16 16">
-            <path d="M14 0a2 2 0 0 1 2 2v12.793a.5.5 0 0 1-.854.353l-2.853-2.853a1 1 0 0 0-.707-.293H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12z"/>
-          </svg>
+        <hr class="my-0" style="opacity: 0.1;">
+
+
+        <div class="row text-secondary g-0 fw-bold" role="button" style="user-select: none; font-size: 14px;">
+          <div class="col text-center py-3 guide-3">
+            <svg xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="20"
+                fill="currentColor"
+                viewBox="0 0 16 16">
+                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+            </svg>
+            Me encanta
+          </div>
+          <div class="col text-center py-3">
+            <svg xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="20"
+                fill="currentColor"
+                viewBox="0 0 16 16">
+                <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"></path>
+            </svg>
+            Comentar
+          </div>
+        </div>
+
     </div>
   </div>
 
