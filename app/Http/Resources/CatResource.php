53cf9be4fb43b948b01f9462ba7ea4c4f2f7ecf0
Amilcar Laura <denotefull@gmail.com>
2021-12-24 10:47:47 -0500

    Crear resource para cat model

--- app/Http/Resources/CatResource.php
+++ app/Http/Resources/CatResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class UserResource extends JsonResource
+class CatResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -15,11 +15,10 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
-            "id" => $this->id,
             "name" => $this->name,
+            "nickname" => $this->nickname,
             'image' => new ImageResource($this->whenLoaded('image')),
-            "username" => $this->username,
-            "url" => "/@$this->username",
+            'user' => new ImageResource($this->whenLoaded('user')),
         ];
     }
 }
