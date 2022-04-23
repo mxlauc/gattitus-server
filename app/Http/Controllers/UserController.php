089809fb910a6d7351b6f4f7c7a71a4a20b50473
Amilcar Laura <denotefull@gmail.com>
2022-04-22 21:23:26 -0500

    Agregar user get api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        return UserResource::collection(User::with("image")->limit(3)->get());
     }
 
     /**
