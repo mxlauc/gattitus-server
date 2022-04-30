47f71fbe56f524f4fed5906d76a72fab91a88549
Amilcar Laura <denotefull@gmail.com>
2022-04-29 20:56:28 -0500

    Aumentar items de paginador de comentarios

--- app/Http/Controllers/PostCommentController.php
+++ app/Http/Controllers/PostCommentController.php
@@ -26,7 +26,7 @@ class PostCommentController extends Controller
     public function index(Request $request, $id)
     {
         $this->authorize(PostComment::class);
-        return new PostCommentCollection(PostComment::with('user.image')->where('post_id', $id)->orderBy('id', 'desc')->cursorPaginate(3));
+        return new PostCommentCollection(PostComment::with('user.image')->where('post_id', $id)->orderBy('id', 'desc')->cursorPaginate(5));
 
     }
 
