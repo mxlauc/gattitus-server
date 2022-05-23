ff8bd1831896ef14b6dcb7d567420af93281ca58
Amilcar Laura <denotefull@gmail.com>
2022-05-22 19:14:19 -0500

    Agregar pet discover

---
+++ app/Http/Controllers/PetDiscoverController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\PetResource;
+use App\Models\Pet;
+use Illuminate\Http\Request;
+
+class PetDiscoverController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        return PetResource::collection(
+            Pet::with('image', 'petType')
+                    ->where('user_id', '!=', $request->user()->id)
+                    ->inRandomOrder()
+                    ->limit(6)
+                    ->get()
+        );
+    }
+}
